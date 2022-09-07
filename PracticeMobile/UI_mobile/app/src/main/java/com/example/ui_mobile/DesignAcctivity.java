7cdc33f10198f6b43f1eb9b18e53e6b97283a2fa
Nguyễn Đức Cường <ndcqn02@gmail.com>
2022-09-07 17:51:29 +0700

    nop BMI

---
+++ PracticeMobile/UI_mobile/app/src/main/java/com/example/ui_mobile/DesignAcctivity.java
@@ -0,0 +1,13 @@
+package com.example.ui_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DesignAcctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design_acctivity);
+    }
+}
