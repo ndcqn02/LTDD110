08482be74c7ad5423a9199b8dc7fb0f8db3ce58e
Nguyễn Đức Cường <ndcqn02@gmail.com>
2022-09-12 01:25:32 +0700

    profile screen

---
+++ PracticeMobile/Profilescreen/app/src/main/java/com/example/profilescreen/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.profilescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {  // ẩn action bar
+            getSupportActionBar().hide();
+        }
+
+    }
+}
