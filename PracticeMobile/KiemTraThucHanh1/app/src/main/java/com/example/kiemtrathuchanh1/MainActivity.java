0f18f60af825aa0a0f5a04e0d06eefcac9e95580
Nguyễn Đức Cường <ndcqn02@gmail.com>
2022-09-21 20:28:03 +0700

    tinh nang

--- PracticeMobile/KiemTraThucHanh1/app/src/main/java/com/example/kiemtrathuchanh1/MainActivity.java
+++ PracticeMobile/KiemTraThucHanh1/app/src/main/java/com/example/kiemtrathuchanh1/MainActivity.java
@@ -12,37 +12,50 @@ import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
 
-    String ten;
-    String msv;
-    String que;
 
-    EditText hten = findViewById(R.id.editTextTen);
-    EditText msvien = findViewById(R.id.editTextMSV);
-    EditText queQuan = findViewById(R.id.editTextQueQuan);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Button btnChuyen = findViewById(R.id.btnMain);
+        Button btnCancel = findViewById(R.id.btnCancel);
         btnChuyen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ten = hten.getText().toString();
-                msv = msvien.getText().toString();
-                que = queQuan.getText().toString();
+                EditText hten = (EditText) findViewById(R.id.editTextTen);
+                EditText msvien = (EditText) findViewById(R.id.editTextMSV);
+                EditText queQuan = (EditText) findViewById(R.id.editTextQueQuan);
+
+
+                String ten = hten.getText().toString();
+                String msv = msvien.getText().toString();
+                String que = queQuan.getText().toString();
 
 
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 SinhVien sv = new SinhVien(ten, msv, que);
-                intent.putExtra("dulie",  sv);
+                intent.putExtra("dulieu",  sv);
 
                 startActivity(intent);
 
+            }
+        });
 
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText hten = (EditText) findViewById(R.id.editTextTen);
+                EditText msvien = (EditText) findViewById(R.id.editTextMSV);
+                EditText queQuan = (EditText) findViewById(R.id.editTextQueQuan);
 
-
+                hten.setText("");
+                msvien.setText("");
+                queQuan.setText("");
             }
         });
     }
--- PracticeMobile/KiemTraThucHanh1/app/src/main/java/com/example/kiemtrathuchanh1/MainActivity2.java
+++ PracticeMobile/KiemTraThucHanh1/app/src/main/java/com/example/kiemtrathuchanh1/MainActivity2.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
-    TextView ten = (TextView) findViewById(R.id.txtten);
-    TextView msvien = (TextView)findViewById(R.id.txtMSV);
-    TextView queQuan = (TextView) findViewById(R.id.txtquequan);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        TextView ten = (TextView) findViewById(R.id.txtten);
+        TextView msvien = (TextView)findViewById(R.id.txtMSV);
+        TextView queQuan = (TextView) findViewById(R.id.txtquequan);
+
 
         Intent intent = getIntent();
         SinhVien sv2 = (SinhVien) intent.getSerializableExtra("dulieu");
 
+        ten.setText(sv2.getTen());
+        msvien.setText( " " + sv2.getMsv() );
+        queQuan.setText(" " + sv2.getQuequan() );
+
+        Button btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
+
 }
