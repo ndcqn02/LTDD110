69eebe04e698e5a40ee9b5c340c4459ab6c62aa4
Nguyễn Đức Cường <ndcqn02@gmail.com>
2022-10-26 19:52:07 +0700

    add file adapter

--- PracticeMobile/Reference/app/src/main/java/com/example/reference/ContactsAdapter.java
+++ PracticeMobile/Reference/app/src/main/java/com/example/reference/ContactsAdapter.java
@@ -1,45 +1,53 @@
 package com.example.reference;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.ViewGroup;
+import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter> {
-    List<Contacts> list;
-
-    public void setData(List<Contacts> list){
-        this.list = list;
-        notifyDataSetChanged();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacts_adapter);
-
-
-
-    }
-
-
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+    private List<Contacts> list;
     @NonNull
     @Override
-    public ContactsAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ContactsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ContactsAdapter holder, int position) {
-
+    public void onBindViewHolder(@NonNull ContactsAdapter.ViewHolder holder, int position) {
+        Contacts contact = list.get(position);
+        if (contact ==  null)
+            return;
+        holder.nameContact.setText(contact.getName());
+        holder.numberContact.setText(contact.getNumbers());
     }
 
     @Override
     public int getItemCount() {
+        if (list != null){
+            return list.size();
+        }
         return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameContact;
+        private TextView numberContact;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameContact = itemView.findViewById(R.id.nameContact);
+            numberContact = itemView.findViewById(R.id.numberContact);
+
+        }
     }
 }
